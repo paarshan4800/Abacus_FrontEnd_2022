03cc6c77266c56e71e8cea56342553138ade628d
seshathilak <seshathilak02@gmail.com>
2022-02-19 12:32:41 +0530

    removed unwanted files

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -1,42 +1,41 @@
 const initialState = {
-    auth: false,
-    width: "SDFJSDF",
-    height: 0,
-    newload: true,
-    newloadworkshop: true,
-  };
-  
-  const Reducer = (state = initialState, action) => {
-    switch (action.type) {
-      case "SETAUTH":
-        return {
-          ...state,
-          auth: action.auth,
-        };
-      case "SETWIDTH":
-        return {
-          ...state,
-          width: action.width,
-        };
-      case "SETHEIGHT":
-        return {
-          ...state,
-          height: action.height,
-        };
-      case "SETNEWLOAD":
-        return {
-          ...state,
-          newload: action.newload,
-        };
-      case "SETNEWLOADWORKSHOP":
-        return {
-          ...state,
-          newloadworkshop: action.newloadworkshop,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default Reducer;
-  
+  auth: false,
+  width: 0,
+  height: 0,
+  newload: true,
+  newloadworkshop: true,
+};
+
+const Reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "SETAUTH":
+      return {
+        ...state,
+        auth: action.auth,
+      };
+    case "SETWIDTH":
+      return {
+        ...state,
+        width: action.width,
+      };
+    case "SETHEIGHT":
+      return {
+        ...state,
+        height: action.height,
+      };
+    case "SETNEWLOAD":
+      return {
+        ...state,
+        newload: action.newload,
+      };
+    case "SETNEWLOADWORKSHOP":
+      return {
+        ...state,
+        newloadworkshop: action.newloadworkshop,
+      };
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
