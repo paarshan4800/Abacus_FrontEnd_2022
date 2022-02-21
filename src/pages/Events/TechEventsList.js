b6a5132062d6d08f5653db1fe258933dfe90df93
AnusreeV1 <anusree9578@gmail.com>
2022-02-21 23:11:14 +0530

    Modified events list

--- src/pages/Events/TechEventsList.js
+++ src/pages/Events/TechEventsList.js
@@ -1,29 +1,45 @@
 import { Carousel } from "3d-react-carousal";
 import React from "react";
+import cx from "classnames";
 import Button from "react";
 import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
-import TechEventsList from "./TechEventsList";
+//import TechEventsList from "./TechEventsList";
 import background from "./../images/bg1.jpg";
 import tech from "./../images/tech.jpeg";
 import nontech from "./../images/nontech.jpeg";
 import hack from "./../images/hack.jpeg";
-import styles from "./EventsHome.module.css";
+import styles from "./TechEventsList.module.css";
 import { useHistory } from "react-router-dom";
 
-function EventsHome() {
+function TechEventsList() {
   let techEvents = [
     {
       id: 1,
-      name: "Asdfgh",
+      name: "Tenet",
       img: tech,
     },
     {
       id: 2,
-      name: "qwerty",
+      name: "Code for crown",
       img: tech,
     },
     {
       id: 3,
+      name: "Tenet",
+      img: tech,
+    },
+    {
+      id: 4,
+      name: "Asdfgh",
+      img: tech,
+    },
+    {
+      id: 5,
+      name: "qwerty",
+      img: tech,
+    },
+    {
+      id: 6,
       name: "zxcvb",
       img: tech,
     },
@@ -37,24 +53,49 @@ function EventsHome() {
 
   return (
     <div
-      style={{
-        backgroundImage: `url(${background})`,
-        backgroundSize: "cover",
-        height: "100vh",
-        color: "#f5f5f5",
-      }}
+    // style={{
+    //   backgroundImage: `url(${background})`,
+    //   backgroundSize: "cover",
+    //   height: "100vh",
+    //   color: "#f5f5f5",
+    // }}
     >
-      <div className={styles.displaylist}>
-        {techEvents.map((event) => (
-          <Link to={`/events/tech-events/${event.id}`}>
-            <div key={event.id}>
-              <img src={event.img} alt="1" />
-              <h1 className={styles.neon}>{event.name}</h1>
-            </div>
-          </Link>
-        ))}
+      <div className={styles.wrapper}>
+        {techEvents.map((event) =>
+          event.id % 3 == 2 ? (
+            <Link to={`/events/tech-events/${event.id}`}>
+              <div
+                key={event.id}
+                className={cx(styles.maindiv, styles.maindiv2)}
+              >
+                <img src={event.img} alt="1" />
+                <div className={styles.title}>{event.name}</div>
+              </div>
+            </Link>
+          ) : event.id % 3 == 1 ? (
+            <Link to={`/events/tech-events/${event.id}`}>
+              <div
+                key={event.id}
+                className={cx(styles.maindiv, styles.maindiv1)}
+              >
+                <img src={event.img} alt="1" />
+                <div className={styles.title}>{event.name}</div>
+              </div>
+            </Link>
+          ) : (
+            <Link to={`/events/tech-events/${event.id}`}>
+              <div
+                key={event.id}
+                className={cx(styles.maindiv, styles.maindiv3)}
+              >
+                <img src={event.img} alt="1" />
+                <div className={styles.title}>{event.name}</div>
+              </div>
+            </Link>
+          )
+        )}
       </div>
     </div>
   );
 }
-export default EventsHome;
+export default TechEventsList;
