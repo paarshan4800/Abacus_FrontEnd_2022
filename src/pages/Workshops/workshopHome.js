34ff25f92408917f3bb96c729dcc5c7064720efe
seshathilak <seshathilak02@gmail.com>
2022-02-21 21:15:03 +0530

    nvm

--- src/pages/Workshops/workshopHome.js
+++ src/pages/Workshops/workshopHome.js
@@ -6,7 +6,7 @@ import background from "./../images/bg1.jpg";
 import tech from "./../images/tech.jpeg";
 import nontech from "./../images/nontech.jpeg";
 import hack from "./../images/hack.jpeg";
-import styles from "./WorkshopHome.module.css";
+import styles from "./workshopHome.module.css";
 
 function WorkshopHome() {
   let slides = [
