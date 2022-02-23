3a30bbb34bae4de4e92443e057f79d3910faeecf
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2022-02-23 13:44:11 +0530

    sign up and verify email

--- src/pages/Login/LoginRegister.js
+++ src/pages/Login/LoginRegister.js
@@ -7,40 +7,46 @@ import ForgotPassword from "./pages/ForgotPassword";
 import logstyle from "./logstyle.module.css";
 import NewPassword from "./pages/NewPassword";
 import GlassButton from "../../components/GlassButton/GlassButton";
-function LoginRegister()
-{
-    return (
-      <Router>
+import VerifyEmail from "./pages/VerifyEmail";
+function LoginRegister() {
+  return (
+    <Router>
       <div className={logstyle.App}>
-       {/*<div className={logstyle.appAside} ><center><div className={logstyle.typed-out}>Welcome!</div></center></div>*/}
-       <div className={logstyle.appAside} >
-        <center>
-          <Typography>
-            <div className={logstyle.typingDemo} width="40%">
-              <div className={logstyle.glow}>
-                Welcome!
+        {/*<div className={logstyle.appAside} ><center><div className={logstyle.typed-out}>Welcome!</div></center></div>*/}
+        <div className={logstyle.appAside}>
+          <center>
+            <Typography>
+              <div className={logstyle.typingDemo} width="40%">
+                <div className={logstyle.glow}>Welcome!</div>
               </div>
-            </div>
-          </Typography>
-        </center>
+            </Typography>
+          </center>
         </div>
-          <div className={logstyle.appForm}>
-            <div className={logstyle.styleButton}>
-              <NavLink to="/sign-in" activeclassName={logstyle.pageSwitcherItemActive} >
-                <GlassButton title="Sign In" />
-              </NavLink>
-              <NavLink exact to="/" activeclassName={logstyle.pageSwitcherItemActive} >
-                <GlassButton title="Sign Up" />
-              </NavLink>
-            </div>
-            <Route path="/NewPassword" component = {NewPassword}/>
-            <Route exact path="/" component={SignUp} />   
-            <Route path="/sign-in" component={SignInForm} />
-            <Route path="/ForgotPassword" component={ForgotPassword} />
+        <div className={logstyle.appForm}>
+          <div className={logstyle.styleButton}>
+            <NavLink
+              to="/sign-in"
+              activeclassName={logstyle.pageSwitcherItemActive}
+            >
+              <GlassButton title="Sign In" />
+            </NavLink>
+            <NavLink
+              exact
+              to="/"
+              activeclassName={logstyle.pageSwitcherItemActive}
+            >
+              <GlassButton title="Sign Up" />
+            </NavLink>
           </div>
+          <Route path="/NewPassword" component={NewPassword} />
+          <Route exact path="/" component={SignUp} />
+          <Route path="/sign-in" component={SignInForm} />
+          <Route path="/ForgotPassword" component={ForgotPassword} />
+          <Route path="/VerifyEmail" component={VerifyEmail} />
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
+}
 
 export default LoginRegister;
--- src/pages/Login/pages/SignUp.js
+++ src/pages/Login/pages/SignUp.js
@@ -1,160 +1,211 @@
 import React, { Component, useState } from "react";
 import { Link } from "react-router-dom";
-import Select from 'react-select';
+import Select from "react-select";
 import departments from "./assets/departments";
 import years from "./assets/years";
 import colleges from "./assets/colleges";
 import logstyle from "./../logstyle.module.css";
 import GlassButton from "../../../components/GlassButton/GlassButton.js";
+import axios from "axios";
 
-function SignUp()
-{
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [department, setDepartment] = useState("");
-    const [college, setCollege] = useState("");
-    const [password, setPassword] = useState("");
-    const [conpass, setConpass] = useState("");
-    const [year, setYear] = useState("");
-    
-    return (
-      <div className={logstyle.formCenter}>
-        <form /*onSubmit={this.handleSubmit}*/ className={logstyle.formFields}>
-          {/*name*/}
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="name">
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="name"
-              className={logstyle.formFieldInput}
-              placeholder="Enter your full name"
-              name="name"
-              value={name}
-              onChange={(e) => {
-                setName(e.currentTarget.value);
-              }}
-            />
-          </div>
+function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [department, setDepartment] = useState("");
+  const [college, setCollege] = useState("");
+  const [password, setPassword] = useState("");
+  const [conpass, setConpass] = useState("");
+  const [year, setYear] = useState("");
 
-          {/*year*/}
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="year">
-              Year
-            </label>
-            <Select className={logstyle.drop} components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
-            options={years.map(opt => ({ label: opt, value: opt }))}
+  const handleSubmit = () => {
+    var values = {
+      email,
+      name,
+      phoneNumber: phone,
+      college: college.value,
+      year: year.value,
+      department: department.value,
+      password,
+    };
+
+    axios
+      .post("http://localhost:8000/user/signup/newUser", values)
+      .then((response) => {
+        if (response.status === 201) {
+          alert(response.data.message);
+        }
+      })
+      .catch((err) => {
+        if (err.response.data.code === 11000) {
+          alert(
+            err.response.data.keyValue.email +
+              "is already registered. Go to login"
+          );
+          setTimeout(() => {
+            window.location = "http://localhost:3000/Login#/sign-in";
+          }, 1000);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+
+    console.log(values);
+  };
+  return (
+    <div className={logstyle.formCenter}>
+      <form onSubmit={() => handleSubmit()} className={logstyle.formFields}>
+        {/*name*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="name">
+            Full Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            className={logstyle.formFieldInput}
+            placeholder="Enter your full name"
+            name="name"
+            value={name}
+            onChange={(e) => {
+              setName(e.currentTarget.value);
+            }}
+          />
+        </div>
+
+        {/*year*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="year">
+            Year
+          </label>
+          <Select
+            className={logstyle.drop}
+            components={{
+              DropdownIndicator: () => null,
+              IndicatorSeparator: () => null,
+            }}
+            options={years.map((opt) => ({ label: opt, value: opt }))}
             value={year}
             onChange={(e) => {
               setYear(e);
             }}
-            placeholder="Year" />
-          </div>
+            placeholder="Year"
+          />
+        </div>
 
-          {/*department*/}
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="dept">
-              Department
-            </label>
-            <Select className={logstyle.drop} components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
-            options={departments.map(opt => ({ label: opt, value: opt }))}
+        {/*department*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="dept">
+            Department
+          </label>
+          <Select
+            className={logstyle.drop}
+            components={{
+              DropdownIndicator: () => null,
+              IndicatorSeparator: () => null,
+            }}
+            options={departments.map((opt) => ({ label: opt, value: opt }))}
             value={department}
             onChange={(e) => {
               setDepartment(e);
             }}
-            placeholder="Department" />
-          </div>
+            placeholder="Department"
+          />
+        </div>
 
-            {/*college*/}
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="colg">
-              College
-            </label>
-            <Select className={logstyle.drop} components={{ DropdownIndicator: () => null, IndicatorSeparator: () => null }}
-            options={colleges.map(opt => ({ label: opt, value: opt }))} 
+        {/*college*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="colg">
+            College
+          </label>
+          <Select
+            className={logstyle.drop}
+            components={{
+              DropdownIndicator: () => null,
+              IndicatorSeparator: () => null,
+            }}
+            options={colleges.map((opt) => ({ label: opt, value: opt }))}
             value={college}
             onChange={(e) => {
               setCollege(e);
             }}
-            placeholder="College" />
-          </div>
+            placeholder="College"
+          />
+        </div>
 
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="email">
-              E-Mail Address
-            </label>
-            <input
-              type="email"
-              id="email"
-              className={logstyle.formFieldInput}
-              placeholder="Enter your email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.currentTarget.value)}
-            />
-          </div>
-           {/*phone*/}
-           <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="phone">
-              Phone Number
-            </label>
-            <input
-              type="number"
-              id="phone"
-              className={logstyle.formFieldInput}
-              placeholder="Enter your Phone Number"
-              name="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.currentTarget.value)}
-            />
-          </div>
-          <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="password">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              className={logstyle.formFieldInput}
-              placeholder="Enter your password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.currentTarget.value)}
-            />
-          </div>
-           {/*confirm password*/}
-           <div className={logstyle.formField}>
-            <label className={logstyle.formFieldLabel} htmlFor="conpass">
-              Confirm Password
-            </label>
-            <input
-              type="password"
-              id="conpass"
-              className={logstyle.formFieldInput}
-              placeholder="Confirm your password"
-              name="conpass"
-              value={conpass}
-              onChange={(e) => setConpass(e.currentTarget.value)}
-            />
-          </div>
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="email">
+            E-Mail Address
+          </label>
+          <input
+            type="email"
+            id="email"
+            className={logstyle.formFieldInput}
+            placeholder="Enter your email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+        </div>
+        {/*phone*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="phone">
+            Phone Number
+          </label>
+          <input
+            type="text"
+            id="phone"
+            className={logstyle.formFieldInput}
+            placeholder="Enter your Phone Number"
+            name="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.currentTarget.value)}
+          />
+        </div>
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="password">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            className={logstyle.formFieldInput}
+            placeholder="Enter your password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+        </div>
+        {/*confirm password*/}
+        <div className={logstyle.formField}>
+          <label className={logstyle.formFieldLabel} htmlFor="conpass">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="conpass"
+            className={logstyle.formFieldInput}
+            placeholder="Confirm your password"
+            name="conpass"
+            value={conpass}
+            onChange={(e) => setConpass(e.currentTarget.value)}
+          />
+        </div>
 
-
-          <div className={logstyle.formField}>
-            {/*<button className={logstyle.formFieldButton}>Sign Up</button>{" "}*/}
-            <div className={logstyle.styleButton}>
-              <GlassButton title="Sign Up"/>
-            </div>
+        <div className={logstyle.formField}>
+          {/*<button className={logstyle.formFieldButton}>Sign Up</button>{" "}*/}
+          <div className={logstyle.styleButton} onClick={() => handleSubmit()}>
+            <GlassButton title="Sign Up" />
           </div>
-          <div className={logstyle.formField}>
-            <Link to="/sign-in" className={logstyle.formFieldLink}>
-              I'm already a member
-            </Link>
-          </div>
-        </form>
-      </div>
-    );
+        </div>
+        <div className={logstyle.formField}>
+          <Link to="/sign-in" className={logstyle.formFieldLink}>
+            I'm already a member
+          </Link>
+        </div>
+      </form>
+    </div>
+  );
   //}
 }
 export default SignUp;
