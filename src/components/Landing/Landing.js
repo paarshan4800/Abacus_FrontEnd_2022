e32f27a3c4eeaba52cb518f993f887fa299b0c2a
Sanjeev <sanjeevkarthick.smcore@gmail.com>
2022-02-24 22:11:41 +0530

    Mobile

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -19,8 +19,8 @@ class Landing extends Component {
             <div className={`${styles._homepage}`} >
             
             <div className={`${styles.landing_heading} ${styles.head}`} >
-                <h1 className={`${styles.head}`}><span className={`${styles.letter_a} ${styles.anim}`}>A</span>BACUS '22</h1>
-                <h2>March 25 and 26</h2>
+                <h1 className={`${styles.head}`}><span className={`${styles.letter_a} ${styles.anim}`}>A</span>BACUS   '22</h1>
+                <h2 className={`${styles.date}`}>March 25 and 26</h2>
             </div>
             {/*
 
