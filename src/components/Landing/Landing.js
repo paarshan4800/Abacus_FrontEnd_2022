aaa0170b770d281267e80d862c70ab076a4cfd08
Sanjeev <sanjeevkarthick.smcore@gmail.com>
2022-02-26 20:32:16 +0530

    Remove unwanted files

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -19,7 +19,8 @@ class Landing extends Component {
             <div className={`${styles._homepage}`}  >
             
             <div className={`${styles.landing_heading} `} >
-                <h1 className={`${styles.head}`}><span className={`${styles.letter_a} ${styles.anim}`}>A</span>BACUS   '22</h1>
+            <span className={`${styles.letter_a}`}></span>
+                <h1 className={`${styles.head} ${styles.glitch}`}  title="ABACUS   '22">ABACUS   '22</h1>
                 <h2 className={`${styles.date}`}>March 25 and 26</h2>
             </div>
             {/*
