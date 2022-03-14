14bf1fdbf1ffe0f7f6d309c4a6d25e9c408ce92d
Jyotir Aditya Giri <58664120+joe-aditya@users.noreply.github.com>
2022-03-14 10:36:24 +0530

    event details

--- src/pages/Events/EventDetails.js
+++ src/pages/Events/EventDetails.js
@@ -56,17 +56,45 @@ function EventDetails() {
       </div>
 
       <div className={styles.container}>
+      <div className={styles.box} style={{ width: "400px" }}>
+          <span></span>
+          <div className={styles.content}>
+            <img
+              src="../../images/nontech.jpeg"
+              style={{ width: "200px", height: "200px" }}
+            />
+          </div>
+        </div>
+
+      <div className={styles.box} style={{ width: "800px" }}>
+          <span></span>
+          <div className={styles.content}>
+            <h2 className={styles.neon}>DESCRIPTION</h2>
+            <p>{data.about}</p>
+            {/* <a href="#">Read More</a> */}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.container}>
         <div className={styles.box}>
           <span></span>
           <div className={styles.content}>
             <h2 className={styles.neon}>RULES</h2>
-            <p>
-              1) The event is for a team of three participants (At Least 2
-              participants)<br></br> 2) Problem statement will be provided on
-              25th March, 2021 and participants are expected to present their
-              prototype on 26th March, 2021 <br></br>3) Decisions made by the
-              administrators will be final
-            </p>
+            <p>{data.rules}</p>
+
+            <div className="datetime">
+              <h2 className={styles.neon}>DATE & TIME</h2>
+              <p>{data.dateTime}</p>
+
+              <h2 className={styles.neon}>CONTACT</h2>
+              {/* {data.contact.map((person, index) => (
+                <p key={index + 1} className={styles.glowCardName}>
+                  {person.name} - <i class="fa fa-phone"></i>{" "}
+                  <span style={{ color: "#fff" }}>{person.mobile}</span>
+                </p>
+              ))} */}
+            </div>
             {/* <a href="#">Read More</a> */}
           </div>
         </div>
