f1ab1ccbd969a6a54c0e5d60a07dc173642cbe5d
ratchabala <ratchabala@gmail.com>
2022-03-26 17:36:36 +0530

    event details

--- src/pages/Events/EventDetails.js
+++ src/pages/Events/EventDetails.js
@@ -28,36 +28,138 @@ function EventDetails() {
   }
 
   return (
+    // <div
+    //   style={{
+    //     backgroundImage: `url(${background})`,
+    //     backgroundSize: "cover",
+    //     backgroundRepeat: 1,
+    //     //height: "100vh",
+    //     // color: "#f5f5f5",
+    //   }}
+    // >
+    //   <div classNameName={styles.container}>
+    //     <div classNameName={styles.box}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         <h2 classNameName={styles.neon}>DESCRIPTION</h2>
+    //         <p>{data.about}</p>
+    //         {/* <h2 classNameName={styles.neon}>DATE & TIME</h2> */}
+    //         {data.dateTime.map((obj) => (
+    //           <p key={1}> {obj}</p>
+    //         ))}
+    //         <h2 classNameName={styles.neon}>CONTACT</h2>
+    //         {data.contact.map((person, index) => (
+    //           <p key={index + 1} classNameName={styles.glowCardName}>
+    //             {person.name} - <i className="fa fa-phone"></i>
+    //             {person.mobile}
+    //           </p>
+    //         ))}
+    //       </div>
+    //     </div>
+
+    //     <div classNameName={styles.box} style={{ width: "400px" }}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         {type === "tech-events" && (
+    //           <img
+    //             src={require(`./../../images/TechEvents/${title}.png`)}
+    //             style={{ width: "200px", height: "200px" }}
+    //             alt={title}
+    //           />
+    //         )}
+    //         {type === "non-tech-events" && (
+    //           <img
+    //             src={require(`./../../images/NonTechEvents/${title}.png`)}
+    //             style={{ width: "200px", height: "200px" }}
+    //             alt={title}
+    //           />
+    //         )}
+    //         <GlassButton title="Register" />
+    //       </div>
+    //       {/* <GlassButton /> */}
+    //     </div>
+    //   </div>
+
+    //   <div classNameName={styles.container}>
+    //     <div classNameName={styles.box} style={{ width: "400px" }}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         <img src={nontech} style={{ width: "200px", height: "200px" }} />
+    //       </div>
+    //     </div>
+
+    //     <div classNameName={styles.box} style={{ width: "800px" }}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         <h2 classNameName={styles.neon}>SPONSORS</h2>
+    //         <p>{data.about}</p>
+
+    //         {/* <a href="#">Read More</a> */}
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div classNameName={styles.container}>
+    //     <div classNameName={styles.box}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         <h2 classNameName={styles.neon}>RULES</h2>
+    //         {data.rules.map((rule, index) => (
+    //           <p key={index + 1} classNameName={styles.glowCardName}>
+    //             {index + 1}. {rule}
+    //           </p>
+    //         ))}
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div classNameName={styles.container}>
+    //     <div classNameName={styles.box}>
+    //       <span></span>
+    //       <div classNameName={styles.content}>
+    //         <h2 classNameName={styles.neon}>ROUNDS</h2>
+    //         {data.rounds.roundDetails.map((round, index) => (
+    //           <p key={index + 1} classNameName={styles.glowCardName}>
+    //             {round.title} - {round.description}
+    //           </p>
+    //         ))}
+    //         <GlassButton title="Scroll to Top" />
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
     <div
       style={{
-        backgroundImage: `url(${background})`,
+        //backgroundImage: `url(${background})`,
         backgroundSize: "cover",
         backgroundRepeat: 1,
-        //height: "100vh",
-        // color: "#f5f5f5",
       }}
     >
       <div className={styles.container}>
         <div className={styles.box}>
           <span></span>
           <div className={styles.content}>
-            <h2 className={styles.neon}>DESCRIPTION</h2>
+            <h2 classNameName={styles.neon}>DESCRIPTION</h2>
             <p>{data.about}</p>
-            {/* <h2 className={styles.neon}>DATE & TIME</h2> */}
+          </div>
+        </div>
+        <div className={styles.box}>
+          <span></span>
+          <div className={styles.content}>
+            <h2 classNameName={styles.neon}>DATE & TIME</h2>
             {data.dateTime.map((obj) => (
               <p key={1}> {obj}</p>
             ))}
-            <h2 className={styles.neon}>CONTACT</h2>
+            <h2 classNameName={styles.neon}>CONTACT</h2>
             {data.contact.map((person, index) => (
-              <p key={index + 1} className={styles.glowCardName}>
-                {person.name} - <i class="fa fa-phone"></i>
+              <p key={index + 1} classNameName={styles.glowCardName}>
+                {person.name} - <i className="fa fa-phone"></i>
                 {person.mobile}
               </p>
             ))}
           </div>
         </div>
-
-        <div className={styles.box} style={{ width: "400px" }}>
+        <div className={styles.box}>
           <span></span>
           <div className={styles.content}>
             {type === "tech-events" && (
@@ -74,60 +176,32 @@ function EventDetails() {
                 alt={title}
               />
             )}
-            <GlassButton  title="Register"/>
+            <GlassButton title="Register" />
           </div>
-<<<<<<< HEAD
-          <GlassButton />
-=======
-          {/* <GlassButton /> */}
->>>>>>> f5f3466583f1bfa9d29dab5a6b0ec1c16fbcf022
         </div>
       </div>
-
       <div className={styles.container}>
-        <div className={styles.box} style={{ width: "400px" }}>
+        <div className={styles.box}>
           <span></span>
           <div className={styles.content}>
-            <img src={nontech} style={{ width: "200px", height: "200px" }} />
-          </div>
-        </div>
-
-        <div className={styles.box} style={{ width: "800px" }}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>SPONSORS</h2>
+            <h2 classNameName={styles.neon}>SPONSORS</h2>
             <p>{data.about}</p>
-
-            {/* <a href="#">Read More</a> */}
           </div>
         </div>
-      </div>
-
-      <div className={styles.container}>
         <div className={styles.box}>
           <span></span>
           <div className={styles.content}>
-            <h2 className={styles.neon}>RULES</h2>
-            {data.rules.map((rule, index) => (
-              <p key={index + 1} className={styles.glowCardName}>
-                {index + 1}. {rule}
+            <h2 classNameName={styles.neon}>DATE & TIME</h2>
+            {data.dateTime.map((obj) => (
+              <p key={1}> {obj}</p>
+            ))}
+            <h2 classNameName={styles.neon}>CONTACT</h2>
+            {data.contact.map((person, index) => (
+              <p key={index + 1} classNameName={styles.glowCardName}>
+                {person.name} - <i className="fa fa-phone"></i>
+                {person.mobile}
               </p>
             ))}
-          </div>
-        </div>
-      </div>
-
-      <div className={styles.container}>
-        <div className={styles.box}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>ROUNDS</h2>
-            {data.rounds.roundDetails.map((round, index) => (
-              <p key={index + 1} className={styles.glowCardName}>
-                {round.title} - {round.description}
-              </p>
-            ))}
-            <GlassButton title="Scroll to Top" />
           </div>
         </div>
       </div>
