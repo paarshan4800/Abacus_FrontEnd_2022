34754d92e13b458ecbe9d23fcf2ce8fbef3d3d8b
Jyotir Aditya Giri <58664120+joe-aditya@users.noreply.github.com>
2022-03-27 13:36:35 +0530

    Login-notMobileView

--- src/pages/Login/dummy/Main.js
+++ src/pages/Login/dummy/Main.js
@@ -1,71 +1,78 @@
 import { Card } from "@material-ui/core";
 import ReactDOM from "react-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { BrowserRouter, Route, Switch, Redirect, NavLink } from "react-router-dom";
 import logstyle from "./../logstyle.module.css";
 import SignUp from "../pages/SignUp";
 import SignInForm from "../pages/SignInForm";
 import Flippy, { FrontSide, BackSide } from 'react-flippy';
-const FlippyStyle = {
-    width: '60%',
-    height: '60%',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'sans-serif',
-    fontSize: '3vh',
-    justifyContent: 'center'
-  }
 
-  const DefaultCardContents = ({ children }) => (
-    <React.Fragment>
-      <FrontSide
-        style={{
-          backgroundColor: '#11998e',
-          display: 'flex',
-          alignItems: 'center',
-          flexDirection: 'column'
-        }}
-      >
-        <SignUp />
-        <span 
-          style={{
-            fontSize:'12px',
-            position: 'absolute',
-            bottom: '10px',
-            width: '100%',
-            fontFamily: 'sans-serif',
-            fontSize: '3vh'
-          }}>
-          {children}<br />
-          To login
-        </span>
-      </FrontSide>
-      <BackSide
-        style={{
-          backgroundColor: '#38ef7d',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexDirection: 'column'
-        }}>
-        <SignInForm />
-        <span 
-          style={{
-            fontSize:'12px',
-            position: 'absolute',
-            bottom: '10px',
-            width: '100%',
-            fontFamily: 'sans-serif',
-            fontSize: '3vh'
-          }}>
-          {children}<br />
-          To create a new Account
-        </span>
-      </BackSide>
-    </React.Fragment>);
+
+// const FlippyStyle = {
+//     width: width={flipPageWidth},
+//     height: '40%',
+//     textAlign: 'center',
+//     color: '#FFF',
+//     fontFamily: 'sans-serif',
+//     fontSize: '3vh',
+//     justifyContent: 'center',
+//   }
+
+  // const DefaultCardContents = ({ children }) => (
+  //   <React.Fragment>
+  //     <FrontSide
+  //       style={{
+  //         // backgroundColor: '#11998e',
+  //         display: 'flex',
+  //         alignItems: 'center',
+  //         flexDirection: 'column'
+  //       }}
+  //     >
+  //       <SignInForm />
+  //       <span 
+  //         style={{
+  //           fontSize:'12px',
+  //           position: 'absolute',
+  //           bottom: '10px',
+  //           width: '100%',
+  //           fontFamily: 'sans-serif',
+  //           fontSize: '3vh'
+  //         }}>
+  //         {children}<br />
+  //         To signUp
+  //       </span>
+  //     </FrontSide>
+  //     <BackSide
+  //       style={{
+  //         // backgroundColor: '#38ef7d',
+  //         display: 'flex',
+  //         alignItems: 'center',
+  //         justifyContent: 'center',
+  //         flexDirection: 'column'
+  //       }}>
+  //       <SignUp />
+  //       <span 
+  //         style={{
+  //           fontSize:'12px',
+  //           position: 'absolute',
+  //           bottom: '10px',
+  //           width: '100%',
+  //           fontFamily: 'sans-serif',
+  //           fontSize: '3vh'
+  //         }}>
+  //         {children}<br />
+  //         To Login
+  //       </span>
+  //     </BackSide>
+  //   </React.Fragment>);
 
 class Main extends React.Component{
+  
+
     constructor() {
+      // this.state={
+      //   width: 0
+      // };
         super();
     }
     render(){
@@ -74,12 +81,43 @@ class Main extends React.Component{
             <Flippy
               ref={(r) => this.flippyHorizontal = r}
               flipOnClick={false}
-              style={FlippyStyle}
+               style={{width: '60%',
+              height: '40%',
+              textAlign: 'center',
+              color: '#FFF',
+              fontFamily: 'sans-serif',
+              fontSize: '3vh',
+              justifyContent: 'center'}}
             >
-            <DefaultCardContents>
+              {/* 
+              <DefaultCardContents>
               <input type="button" value="Click here" style={{fontFamily: 'sans-serif',
     fontSize: '3vh'}} onClick={() => this.flippyHorizontal.toggle()}></input>
-            </DefaultCardContents>
+            </DefaultCardContents> */}
+          <FrontSide
+          style={{
+          // backgroundColor: '#11998e',
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column'
+          }}
+          >
+        <input type="button" value="To SignUp" style={{fontFamily: 'sans-serif',
+    fontSize: '3vh'}} onClick={() => this.flippyHorizontal.toggle()}></input>
+        <SignInForm />
+      </FrontSide>
+      <BackSide
+        style={{
+          // backgroundColor: '#38ef7d',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column'
+        }}>
+        <input type="button" value="To Login" style={{fontFamily: 'sans-serif',
+    fontSize: '3vh'}} onClick={() => this.flippyHorizontal.toggle()}></input>
+        <SignUp />
+      </BackSide>
           </Flippy>
           </div>
         );
