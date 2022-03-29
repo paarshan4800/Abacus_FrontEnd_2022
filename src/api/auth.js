d9ab12367a0beb9a1bd52c1925204d1f8fe819c4
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2022-03-29 12:00:46 +0530

    Update auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,7 +2,7 @@ import { responsiveFontSizes } from "@material-ui/core";
 import axios from "axios";
 
 // const BASE_API_URL = "https://abacus-22-backend.herokuapp.com/";
-const BASE_API_URL = "https://localhost:800";
+const BASE_API_URL = "https://localhost:8000";
 
 export const googleSignIn = () => {
   window.location = BASE_API_URL + "user/login/google";
