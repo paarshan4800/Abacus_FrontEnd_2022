511abc15ecb11df29b721154673e7e6be6451239
ratchabala <ratchabala@gmail.com>
2022-04-08 15:05:54 +0530

    Added FadeInSection to events

--- src/pages/Events/EventDetails.js
+++ src/pages/Events/EventDetails.js
@@ -9,6 +9,7 @@ import GlassBtn from "../../components/GlassBtn/GlassBtn";
 import background from "../../images/bgg.jpg";
 import nontech from "./../../images/nontech.png";
 import { eventRegistration } from "../../api/registrations";
+import FadeInSection from "../../components/FadeInSection/FadeInSection";
 
 function EventDetails() {
   const { type, title } = useParams();
@@ -35,104 +36,112 @@ function EventDetails() {
         backgroundRepeat: 1,
       }}
     >
-      <div className={styles.container}>
-        <div className={styles.box} style={{ height: "auto", width: "30%" }}>
-          <span></span>
-          <div className={styles.content}>
-            {type === "tech-events" && (
-              <img
-                src={require(`./../../images/TechEvents/${title}.png`)}
-                style={{ width: "200px", height: "200px" }}
-                alt={title}
+      <FadeInSection>
+        <div className={styles.container}>
+          <div className={styles.box} style={{ height: "auto", width: "30%" }}>
+            <span></span>
+            <div className={styles.content}>
+              {type === "tech-events" && (
+                <img
+                  src={require(`./../../images/TechEvents/${title}.png`)}
+                  style={{ width: "200px", height: "200px" }}
+                  alt={title}
+                />
+              )}
+              {type === "non-tech-events" && (
+                <img
+                  src={require(`./../../images/NonTechEvents/${title}.png`)}
+                  style={{ width: "200px", height: "200px" }}
+                  alt={title}
+                />
+              )}
+              <GlassButton
+                title="Register"
+                onClick={() => {
+                  eventRegistration();
+                }}
               />
-            )}
-            {type === "non-tech-events" && (
-              <img
-                src={require(`./../../images/NonTechEvents/${title}.png`)}
-                style={{ width: "200px", height: "200px" }}
-                alt={title}
-              />
-            )}
-            <GlassButton
-              title="Register"
-              onClick={() => {
-                eventRegistration();
-              }}
-            />
+            </div>
+            {/* <GlassButton /> */}
           </div>
-          {/* <GlassButton /> */}
-        </div>
-        <div className={styles.box} style={{ height: "auto", width: "60%" }}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>{data.name}</h2>
-            <p>{data.about}</p>
-            {/* <h2 className={styles.neon}>DATE & TIME</h2> */}
-            {/* {data.dateTime.map((obj) => (
+          <div className={styles.box} style={{ height: "auto", width: "60%" }}>
+            <span></span>
+            <div className={styles.content}>
+              <h2 className={styles.neon}>{data.name}</h2>
+              <p>{data.about}</p>
+              {/* <h2 className={styles.neon}>DATE & TIME</h2> */}
+              {/* {data.dateTime.map((obj) => (
               <p key={1}> {obj}</p>
             ))} */}
-            {/* <h2 className={styles.neon}>CONTACT</h2> */}
-            {/* {data.contact.map((person, index) => (
+              {/* <h2 className={styles.neon}>CONTACT</h2> */}
+              {/* {data.contact.map((person, index) => (
               <p key={index + 1} className={styles.glowCardName}>
                 {person.name} - <i class="fa fa-phone"></i>
                 {person.mobile}
               </p>
             ))} */}
+            </div>
           </div>
         </div>
-      </div>
+      </FadeInSection>
 
-      <div className={styles.container}>
-        <div className={styles.box} style={{ height: "auto", width: "60%" }}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>SPONSORS</h2>
-            <img src={nontech} style={{ width: "200px", height: "200px" }} />
+      <FadeInSection>
+        <div className={styles.container}>
+          <div className={styles.box} style={{ height: "auto", width: "60%" }}>
+            <span></span>
+            <div className={styles.content}>
+              <h2 className={styles.neon}>SPONSORS</h2>
+              <img src={nontech} style={{ width: "200px", height: "200px" }} />
 
-            {/* <a href="#">Read More</a> */}
+              {/* <a href="#">Read More</a> */}
+            </div>
           </div>
         </div>
-      </div>
+      </FadeInSection>
 
-      <div className={styles.container}>
-        <div className={styles.box} style={{ height: "auto", width: "auto" }}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>RULES</h2>
-            {data.rules.map((rule, index) => (
-              <p key={index + 1} className={styles.glowCardName}>
-                ► {rule}
-              </p>
-            ))}
-            <p>Team size: {data.teamSize}</p>
-            <p>Rounds: {data.roundsNumber}</p>
+      <FadeInSection>
+        <div className={styles.container}>
+          <div className={styles.box} style={{ height: "auto", width: "auto" }}>
+            <span></span>
+            <div className={styles.content}>
+              <h2 className={styles.neon}>RULES</h2>
+              {data.rules.map((rule, index) => (
+                <p key={index + 1} className={styles.glowCardName}>
+                  ► {rule}
+                </p>
+              ))}
+              <p>Team size: {data.teamSize}</p>
+              <p>Rounds: {data.roundsNumber}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </FadeInSection>
 
-      <div className={styles.container}>
-        <div className={styles.box} style={{ height: "auto", width: "auto" }}>
-          <span></span>
-          <div className={styles.content}>
-            <h2 className={styles.neon}>ROUNDS</h2>
-            {data.rounds.roundDetails.map((round, index) => (
-              <p key={index + 1} className={styles.glowCardName}>
-                <h4>
-                  <b>{round.title}</b>
-                </h4>
-                {round.description.map((desc, index) => (
-                  <p key={index + 1} className={styles.glowCardName}>
-                    ► {desc}
-                  </p>
-                ))}
-                {/* {round.description} */}
-              </p>
-            ))}
+      <FadeInSection>
+        <div className={styles.container}>
+          <div className={styles.box} style={{ height: "auto", width: "auto" }}>
+            <span></span>
+            <div className={styles.content}>
+              <h2 className={styles.neon}>ROUNDS</h2>
+              {data.rounds.roundDetails.map((round, index) => (
+                <p key={index + 1} className={styles.glowCardName}>
+                  <h4>
+                    <b>{round.title}</b>
+                  </h4>
+                  {round.description.map((desc, index) => (
+                    <p key={index + 1} className={styles.glowCardName}>
+                      ► {desc}
+                    </p>
+                  ))}
+                  {/* {round.description} */}
+                </p>
+              ))}
 
-            {/* <GlassButton title="Scroll to Top" /> */}
+              {/* <GlassButton title="Scroll to Top" /> */}
+            </div>
           </div>
         </div>
-      </div>
+      </FadeInSection>
     </div>
   );
 }
