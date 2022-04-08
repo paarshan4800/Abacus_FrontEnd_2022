96b8b31ea3c3021c3320e5b5ebc383f85b6e0e0e
preetikrish19 <62648562+preetikrish19@users.noreply.github.com>
2022-04-09 01:34:52 +0530

    DETAILS ADDED

--- src/pages/Developers/details.js
+++ src/pages/Developers/details.js
@@ -4,34 +4,55 @@ export const developerDetails = [
         title1: "Frontend",
         img: "dev/Preeti.jpg",
         href:
-          "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+          "https://www.linkedin.com/in/preeti-krishnaveni-a1b9201aa/",
     },
     {
         title: "Sneha S",
         title1: "Frontend",
         img: "dev/Sneha S.png",
         href:
-          "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+          "https://www.linkedin.com/in/sneha-s-0766761ab/?trk=public_profile_browsemap&originalSubdomain=in",
     },
     {
         title: "Pranava Raman BMS",
         title1: "Backend",
         img: "dev/Pranava Raman BMS.jpg",
         href:
-          "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+          "https://www.linkedin.com/in/pranava-raman-bms/",
     },
     {
         title: "Ajitesh M",
         title1: "Backend",
         img: "dev/Ajitesh M.jpg",
         href:
-          "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+          "https://www.linkedin.com/in/ajitesh-mahalingam",
     },
     {
         title: "Bharath M",
         title1: "Design",
         img: "dev/Bharath M.png",
         href:
-          "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+          "https://www.linkedin.com/in/bharath-mohan-65a51020b/",
     },
+    {
+      title: "Shafeeq Ur Rahman P A",
+      title1: "Frontend",
+      img: "dev/Shafeeq Ur Rahman P A.png",
+      href:
+        "https://www.linkedin.com/in/darrshana-rajkumar-6a36aa1ba",
+  },
+  {
+    title: "Safiya Fathima Syed",
+    title1: "Backend",
+    img: "dev/Safiya Fathima Syed.png",
+    href:
+      "https://www.linkedin.com/in/safiya-fathima-syed",
+  },
+  {
+    title: "Jyotir Aditya Giri A",
+    title1: "Frontend",
+    img: "dev/Jyotir Aditya Giri A.png",
+    href:
+      "https://www.linkedin.com/in/jyotir-aditya-giri",
+},
 ]
