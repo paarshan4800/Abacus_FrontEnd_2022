df651a5adef368da7963c564b71b63d26ed10497
bmspr1502 <57596428+bmspr1502@users.noreply.github.com>
2022-04-10 10:18:22 +0530

    build images and developers beter

--- src/pages/Developers/Developers.js
+++ src/pages/Developers/Developers.js
@@ -7,16 +7,17 @@ import Dev from "../../components/Dev/Dev";
 import FadeInSection from "../../components/FadeInSection/FadeInSection";
 import { developerDetails } from "./details";
 export default function Developers() {
+  console.log(process.env);
   return (
     <div>
       <div style={{ textAlign: "center" }}>
         <Heading text="Developers and Design Team" fontSize="1.9rem" />
       </div>
-      <div className="container-fluid m-auto d-flex justify-content-center pt-5 align-self-center pb-5">
+      <div className="container m-auto d-flex justify-content-center pt-5 align-self-center pb-5">
         <div className="row d-flex justify-content-center align-self-center pb-5">
           {developerDetails.map((item, index) => {
             return (
-              <div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+              <div className="col-xs-12 col-sm-6 col-md-4 col-lg-2">
                 <FadeInSection>
                   <Dev props={item} />
                 </FadeInSection>
